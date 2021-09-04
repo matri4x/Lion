19b7fbaee4c23d1cec4854868c2f5578ee39be91
matri4x <73620290+matri4x@users.noreply.github.com>
2021-09-04 03:09:17 +0300

    Delete userbot directory

--- userbot/helpers/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import emojify, fonts
-from .aria2utils import *
-from .exceptions import CancelProcess
-from .functions import *
-from .memeifyhelpers import *
-from .progress import *
-from .qhelper import process
